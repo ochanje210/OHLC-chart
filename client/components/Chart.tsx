5fc10d7942b415e725d38531f09436a48a56501d
Youngchan Je <ochanje210@gmail.com>
2020-09-06 02:10:13 +0900

    📊📊📊📊📊📊📊📊📊📊📊📊

---
+++ client/components/Chart.tsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useRef } from 'react';
+import { ChartingLibraryWidgetOptions, widget, IChartingLibraryWidget } from 'shared/chartingLibrary/charting_library.min';
+import { datafeed } from 'shared/chartingLibraryUtils/datafeed';
+import styles from '../styles/Home.module.css';
+
+const tvChartContainerId = 'tv_chart_container'
+
+export default function Chart() {
+  const tvWidget = useRef<IChartingLibraryWidget>()
+
+  useEffect(() => {
+    const widgetOptions: ChartingLibraryWidgetOptions = {
+      symbol: 'BFX/BTC',
+      container_id: tvChartContainerId,
+      datafeed,
+      interval: '1D',
+      locale: 'ko'
+    }
+
+    tvWidget.current = new widget(widgetOptions)
+
+    tvWidget.current.onChartReady(() => {
+			tvWidget.current.headerReady().then(() => {
+				const button = tvWidget.current.createButton();
+				button.setAttribute('title', 'Click to show a notification popup');
+				button.classList.add('apply-common-tooltip');
+				button.addEventListener('click', () => tvWidget.current.showNoticeDialog({
+						title: 'Notification',
+						body: 'TradingView Charting Library API works correctly',
+						callback: () => {
+							console.log('Noticed!');
+						},
+					}));
+				button.innerHTML = 'Check API';
+			});
+		});
+
+    return () => {
+      if (tvWidget.current) {
+        tvWidget.current.remove();
+        tvWidget.current = null;
+      }
+    }
+  }, [])
+
+  return (
+    <div className={styles.container}>
+      <div id={tvChartContainerId}>
+        
+      </div>
+    </div>
+  )
+}
