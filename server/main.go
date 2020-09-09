fe6706026e5fdfc2bbbe9e3b89b69f8de6af141a
Youngchan Je <ochanje210@gmail.com>
2020-09-10 03:19:38 +0900

    허용 도메인 리스트로 관리

--- server/main.go
+++ server/main.go
@@ -12,7 +12,10 @@ import (
 
 var historicCandlestick []byte
 var last1MinTransaction []byte
+
 var upgrader = websocket.Upgrader{}
+
+var allowedOrigins = [2]string{"http://localhost:3000", "http://13.125.151.218:3000"}
 
 func keepFetchingHistoricCandlestick() {
 	for {
@@ -52,15 +55,21 @@ func keepFetchingTransactionHistory() {
 	}
 }
 
-func transactionHistoryHandler(w http.ResponseWriter, r *http.Request) {
-	setCORS(w)
-
-	upgrader.CheckOrigin = func(r *http.Request) bool {
-		origin := r.Header["Origin"]
-		if origin[0] == "http://localhost:3000" {
+func isAllowedOrigin(r *http.Request) bool {
+	origin := r.Header["Origin"]
+	for _, allowedOrigin := range allowedOrigins {
+		if allowedOrigin == origin[0] {
 			return true
 		}
-		return false
+	}
+	return false
+}
+
+func transactionHistoryHandler(w http.ResponseWriter, r *http.Request) {
+	setCORS(w, r)
+
+	upgrader.CheckOrigin = func(r *http.Request) bool {
+		return isAllowedOrigin(r)
 	}
 
 	c, err := upgrader.Upgrade(w, r, nil)
@@ -80,9 +89,12 @@ func transactionHistoryHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func setCORS(w http.ResponseWriter) {
-	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-	w.Header().Set("Access-Control-Allow-Credentials", "true")
+func setCORS(w http.ResponseWriter, req *http.Request) {
+	if isAllowedOrigin(req) {
+		origin := req.Header["Origin"]
+		w.Header().Set("Access-Control-Allow-Origin", origin[0])
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+	}
 }
 
 func handleRequests() {
@@ -92,7 +104,7 @@ func handleRequests() {
 		io.WriteString(w, "OK")
 	})
 	mux.HandleFunc("/historic", func(w http.ResponseWriter, req *http.Request) {
-		setCORS(w)
+		setCORS(w, req)
 		w.Write(historicCandlestick)
 	})
 	mux.HandleFunc("/transactionHistory", transactionHistoryHandler)
